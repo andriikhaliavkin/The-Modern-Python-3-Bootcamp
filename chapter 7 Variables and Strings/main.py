aeb6bfc4dfcf83d49b7772787d9d7f055efcde7c
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-03-29 23:29:59 +0200

    Update main.py
    concatenation + f-string

--- chapter 7 Variables and Strings/main.py
+++ chapter 7 Variables and Strings/main.py
@@ -1,23 +1,28 @@
-print("init")
-x = 100
-print(x)
-n = "some text"
-print(n)
-d = True
-print(d)
-x = d
-print(x)
-q, w, e = 1, 2, 3
-r = q + w + e
-print(r)
+# print("init")
+# x = 100
+# print(x)
+# n = "some text"
+# print(n)
+# d = True
+# print(d)
+# x = d
+# print(x)
+# q, w, e = 1, 2, 3
+# r = q + w + e
+# print(r)
 
-list_example = [1, 2, 4]
-dict_example = {"a": 2, "b": 4}
+# list_example = [1, 2, 4]
+# dict_example = {"a": 2, "b": 4}
 
-None #like null in another PLs
-name = None
-print(name)
+# None #like null in another PLs
+# name = None
+# print(name)
 
-message = "bla bld\n"
-mountains = "/\\/\\/\\"
-print(mountains)
+# message = "bla bld\n"
+# mountains = "/\\/\\/\\"
+# print(mountains)
+
+name = "Andrii"
+last_name = "Khaliavkin"
+print(name + " " + last_name)
+print(f"{name} {last_name}")
