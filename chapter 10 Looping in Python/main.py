b8c63f8e4f71f89d6d8f1f2d3494ebaf648f9afa
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-04-11 06:24:47 +0200

    Update main.py
    exercise

--- chapter 10 Looping in Python/main.py
+++ chapter 10 Looping in Python/main.py
@@ -7,5 +7,15 @@
 # for i in range(0,50,5):
 #   print(i)
 
-for n in range(3):
-  print(n)
+# for n in range(3):
+#   print(n)
+
+
+# Add up all odd numbers between 10 and 20
+# Store the result in x:
+x = 0
+for i in range(11,21,2):
+  x+=i
+print(x)
+
+# YOUR CODE GOES HERE:
