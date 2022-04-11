3eab71e2f4f3ba25640f14cda01073d5465f43f4
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-04-11 06:13:43 +0200

    Update main.py
    ranges

--- chapter 10 Looping in Python/main.py
+++ chapter 10 Looping in Python/main.py
@@ -2,5 +2,10 @@
 # for i in range(1,11):
 #   print(i)
 
-for l in 'coffee':
-  print(l)
+# for l in 'coffee':
+#   print(l)
+# for i in range(0,50,5):
+#   print(i)
+
+for n in range(3):
+  print(n)
